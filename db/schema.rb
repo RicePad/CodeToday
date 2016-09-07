7fc5da51b159f332cbae9aac0035d5f6d6b2f7bb
Jonathanli <jonathanli@wiredhere.com>
2016-09-07 22:40:25 +0000

    current commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907044052) do
+ActiveRecord::Schema.define(version: 20160907213246) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160907044052) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "image"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
